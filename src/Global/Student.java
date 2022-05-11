559d3c7e728326e55e68330404aadea33f24b2e1
Maxim <pankrukhin.maxim@gmail.com>
2022-05-11 14:22:53 +0300

    ..

--- src/Global/Student.java
+++ src/Global/Student.java
@@ -15,21 +15,42 @@ public class Student<T> {
     String name="", surname ="", telegram = "", vk = "", group = "";
     void post() throws IOException {
         //Добавляем на сервер
+
+
+        /*
         URL url = new URL("https://buldakovn.pythonanywhere.com/addStudent");
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.setRequestMethod("POST");
         connection.setDoOutput(true);
 
-        String temp = "{\"FName\": "+name+",\"LName\": "+surname+",\"VkId\": "+vk+",\"TelegrammId\": "+telegram+",\"Group\": "+group+"}";
-        byte[] out = temp.getBytes(StandardCharsets.UTF_8);
+
+        Map<String, String> postargs = new HashMap<>();
+        postargs.put("{", "");
+        postargs.put("FName:", name + ",");
+        postargs.put("LName:", surname + ",");
+        postargs.put("VkId:", vk + ",");
+        postargs.put("TelegrammId:", telegram + ",");
+        postargs.put("Group:", group + "}");
+
+
+        //String temp = "{\"FName\":"+name+",\"LName\":"+surname+",\"VkId\":"+vk+",\"TelegrammId\":"+telegram+",\"Group\":"+group+"}";
+        byte[] out = postargs.toString().getBytes(StandardCharsets.UTF_8);
         int length = out.length;
 
         connection.setFixedLengthStreamingMode(length);
+        connection.addRequestProperty("User-Agent", "Mozilla/5.0");
         connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+        connection.setConnectTimeout(200);
+        connection.setReadTimeout(200);
         connection.connect();
-        try(OutputStream os = connection.getOutputStream()) {
+
+        try {
+            OutputStream os = connection.getOutputStream();
             os.write(out);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
+
         InputStreamReader isR = new InputStreamReader(connection.getInputStream());
         BufferedReader bfr = new BufferedReader(isR);
         StringBuilder str = new StringBuilder();
@@ -38,6 +59,19 @@ public class Student<T> {
             str.append(line);
         }
         System.out.println(str);
+
+        str = new StringBuilder();
+        url = new URL("https://buldakovn.pythonanywhere.com/getStudents");
+        connection = (HttpURLConnection) url.openConnection();
+        connection.setDoInput(true);
+        connection.setRequestMethod("GET");
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))){
+            for (String str1; (str1 = reader.readLine())!= null;){
+                str.append(str1);
+            }
+        }
+        System.out.println(str);
+        */
     }
 
     void add(int i, String temp){
